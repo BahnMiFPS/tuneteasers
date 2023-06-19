da511480431d1d05ba7d668bd0dfe0c8fef454e9
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-06-20 03:14:53 +0700

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const http = require("http")
 const { Server } = require("socket.io")
+require("dotenv").config()
 const cors = require("cors")
 const { getQuestion, generateRoomQuestions } = require("./utils/getQuestion")
 const {
